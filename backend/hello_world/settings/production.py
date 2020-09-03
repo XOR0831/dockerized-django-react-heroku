78056bc0d8542ced8bac444cad672090d32b4048
XOR0831 <sorianodanielb99@gmail.com>
2020-09-03 14:06:40 +0800

    Production Commit

--- backend/hello_world/settings/production.py
+++ backend/hello_world/settings/production.py
@@ -10,11 +10,11 @@ INSTALLED_APPS.extend(["whitenoise.runserver_nostatic"])
 # Must insert after SecurityMiddleware, which is first in settings/common.py
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
-TEMPLATES[0]["DIRS"] = [os.path.join(BASE_DIR, "../", "frontend", "build")]
+TEMPLATES[0]["DIRS"] = [os.path.join(BASE_DIR, "../../", "frontend", "build")]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "../", "frontend", "build", "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "../../", "frontend", "build", "static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = "/static/"
-WHITENOISE_ROOT = os.path.join(BASE_DIR, "../", "frontend", "build", "root")
+WHITENOISE_ROOT = os.path.join(BASE_DIR, "../../", "frontend", "build", "root")
